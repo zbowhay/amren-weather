2f550081bb8df06a53a9d03c2bfed59aa5fa1b95
Zachary Bowhay <zbowhay@gmail.com>
2018-12-23 16:24:19 -0600

    part 5 done

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -18,6 +18,12 @@ export class HeroService {
     return of(HEROES);
   }
 
+  getHero(id: number): Observable<Hero> {
+    // TODO: write to message service AFTER the hero is fetched.
+    this.writeMessage(`fetched hero id=${id}`);
+    return of(HEROES.find((hero) => hero.id === id));
+  }
+
   private writeMessage(msg: string): void {
     this._messageService.add(`HeroService: ${msg}`);
   }
